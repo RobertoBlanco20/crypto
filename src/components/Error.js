2e042900e4f6a9d325298be6a564fb3724c9615a
Roberto Blanco <86691875+RobertoBlanco20@users.noreply.github.com>
2022-11-11 19:18:37 -0300

    production

--- src/components/Error.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Error = () => {
-    return ( 
-        <p className="bg-red-500 p-2 my-2 text-center text-white font-bold"> Todos los campos son obligatorios.</p>
-     );
-}
- 
-export default Error;
